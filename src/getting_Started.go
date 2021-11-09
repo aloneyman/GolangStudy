26157f3c736c74ece93e3002c61016172b5f2b23
chh <caihonghao@gmail.com>
2021-11-09 23:45:14 +0800

    asd

--- src/getting_Started.go
+++ src/getting_Started.go
@@ -46,16 +46,19 @@ func main() {
 	y := 4
 	x := 3
 	if x := f(); x == 0 {
+		fmt.Println("first")
 		fmt.Println(x)
 	} else if y := g(x); x == y {
+		fmt.Println("second")
 		fmt.Println(x, y)
 	} else {
+		fmt.Println("third")
 		fmt.Println(x, y)
 	}
 	fmt.Println(x, y)
 }
 func f() int {
-	return 2
+	return 3
 }
 func g(x int) int {
 	return x
